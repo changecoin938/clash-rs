8c38c256f6f0486a3ead005a05bda57670435c02
changecoin938 <71965653+changecoin938@users.noreply.github.com>
2026-02-11 20:29:20 +0330

    WS upload fix

--- clash-lib/src/common/http/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod client;
-
-pub use client::*;
-
-pub const DEFAULT_USER_AGENT: &str = concat!("clash-rs/", env!("CARGO_PKG_VERSION"));
--- clash-lib/src/config/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod def;
-pub mod internal;
-mod utils;
-pub use def::DNSListen;
-pub use internal::{InternalConfig as RuntimeConfig, *};
